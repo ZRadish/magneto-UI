8d2fe98ec811ed78654e89049aec257b1affbfc1
AishaFathalla <156489851+AishaFathalla@users.noreply.github.com>
2024-10-31 09:39:58 -0400

    updated mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,15 @@
 // index.js
 import dotenv from 'dotenv';
-//import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import app from './app.js';
 
 dotenv.config();
 
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => {
-//   console.log('Connected to MongoDB');
-// }).catch((error) => {
-//   console.error('MongoDB connection error:', error);
-// });
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('MongoDB connection error:', error);
+});
 
 export default app;
