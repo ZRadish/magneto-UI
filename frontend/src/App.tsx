8df17b7a9a8b9ff439930964049048a9ad9341fb
MalakElsayed09 <elsayedmalak93@gmail.com>
2024-11-07 09:59:36 -0500

    Implemented file upload functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage"; // Import LoginPage
 import SignUpPage from "./pages/SignUpPage";
 import EmailVerificationPage from "./pages/EmailVerificationPage";
+import FileUploadPage from "./pages/FileUploadPage";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/verify-email" element={<EmailVerificationPage />} />
+          <Route path="/upload" element={<FileUploadPage />} />
         </Routes>
       </BrowserRouter>
     </>
