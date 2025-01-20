8de0bfe5b24204f9ab62af6de3124272facace33
hashdbrown <halkhateeb704@gmail.com>
2025-01-19 19:16:33 -0500

    implemented language detection child process

--- backend/routes/magnetoRoutes.js
+++ backend/routes/magnetoRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { runThemeCheck } from '../controllers/magnetoController.js';
 import { runBackButton } from '../controllers/magnetoController.js';
+import { runLanguageDetection } from '../controllers/magnetoController.js';
 
 const router = express.Router();
 
 // POST request to run the Python script
 router.post('/theme-check', runThemeCheck);
 router.post('/back-button', runBackButton);
+router.post('/language-detection', runLanguageDetection);
 
 export default router;
